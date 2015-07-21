6c7d5ce81e5dcad399e014db806ba6940d38d6a1
naeemtawwos <naeem.mailto@gmail.com>
2015-07-21 16:35:38 +0530

    added new collections and functions related to the CRUD operations for the new collections

--- js/database.js
+++ js/database.js
@@ -1,11 +1,14 @@
-
-//tis function initializes the database and the collections.
-//if the database does not exist a new database is created, if it exists the existing database is used
-function initializeDB(){
 var Engine = require('tingodb')(),
     assert = require('assert');
 
 var textileManagerDB = new Engine.Db('dbscript/', {});
+
+
+
+//tis function initializes the database and the collections.
+//if the database does not exist a new database is created, if it exists the existing database is used
+initializeDB();
+function initializeDB(){
 // a collection in Mongo is roughly equalto a table in RDBMS
 var loginCreds_Collection = textileManagerDB.collection("textileManagerDB");
 var	suppliers_Collection = textileManagerDB.collection("textileManagerDB");
@@ -23,17 +26,14 @@ LOT will have
 */
 
 var count_Collection = textileManagerDB.collection("textileManagerDB");
+//will contain a count type and description
 
 
-lot_Collection.insert([
-	{count:"30s", description:"combed"},
-	{count:"34s", description:"SemiCombed"}
-	], function(err,result){
-	assert.equal(null,err);
-	console.log(err);	
-	}
-
-	);
+count_Collection.insert({count:"30s", description:"combed"}, function(err, result) {
+  assert.equal(null, err);
+  console.log(err);
+  console.log(result);
+ });
 
 
 loginCreds_Collection.insert([
@@ -55,25 +55,75 @@ suppliers_Collection.insert(
 }
 
 
-function addRemCustomer(){
+/*
+@args
+add: Whether to add or remove the customer, true denotes add
+cust: the object containing the details of the customer or in case of remove, the id of the customer to be removed
+*/
+function addRemCustomer(add,cust){
+	if(add == true){
+		customers_Collection.insert(cust);
+	}
+	else{
+		customers_Collection.remove({"_id":cust});
+	}
+}
+
+/*
+@args
+add: Whether to add or remove the supplier, true denotes add
+cust: the object containing the details of the supplier or in case of remove, the id of the supplier to be removed
+*/
+function addRemSupplier(add, supplier){
+	if(add == true){
+		suppliers_Collection.insert(cust);
+	}
+	else{
+		suppliers_Collection.remove({"_id":cust});
+	}	
+}
+
+
+/*information related to a supplier can be chanded/update
+	The mongo save method replaces an existing document completely with the new document
+*/
+function updateSupplierInfo(id, cust){
+	suppliers_Collection.save({"_id":id}, cust);
+}
+/*information related to a customer can be chanded/update
+	The mongo save method replaces an existing document completely with the new document
+*/
+function updateCustomerInfo(id,cust ){
+	customers_Collection.save({"_id":id}, cust);
+}
+
+
+
+function addNewLot(lot){
+	lot_Collection.insert(lot);
+}
+
+function addNewCountType(count){
+	count_Collection.insert(count);
+}
+
+
+function deleteUser(){
 
 }
 
-function addNewLot(){
+
+function addUser(){
 
 }
 
 
-function addRemSupplier(){
-
-}
-
-function addNewCountType(){
+function changePassword(){
 
 }
 
 
-
-
-
-
+function errorCallback(err, result) {
+	assert.equal(null,err);
+	console.log(err);
+}
