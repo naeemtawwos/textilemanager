f7d29e7ac3fb0ddd9fb1821450093cb327b516c6
naeemtawwos <naeem.mailto@gmail.com>
2015-07-22 18:36:17 +0530

    controller file that switches windows and calls databse queries

---
+++ js/controller.js
@@ -0,0 +1,34 @@
+
+
+
+function saveCustomer(formData){
+    
+    addRemCustomer(add=true,formData);
+	return true;
+}
+
+
+function login(){
+
+}
+
+
+
+//jquery function to serialize form
+//jquery should be included before this script is included
+$.fn.serializeObject = function()
+{
+    var o = {};
+    var a = this.serializeArray();
+    $.each(a, function() {
+        if (o[this.name] !== undefined) {
+            if (!o[this.name].push) {
+                o[this.name] = [o[this.name]];
+            }
+            o[this.name].push(this.value || '');
+        } else {
+            o[this.name] = this.value || '';
+        }
+    });
+    return o;
+};
