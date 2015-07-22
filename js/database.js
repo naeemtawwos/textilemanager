48db09cbefdc00d2ca592e134c4deaacbd4344ec
naeemtawwos <naeem.mailto@gmail.com>
2015-07-22 18:35:08 +0530

    database queries added

--- js/database.js
+++ js/database.js
@@ -4,16 +4,22 @@ var Engine = require('tingodb')(),
 var textileManagerDB = new Engine.Db('dbscript/', {});
 
 
+var customers_Collection;
+customers_Collection=textileManagerDB.collection("textileManagerDB");
+var suppliers_Collection;
+var loginCreds_Collection;
+var lot_Collection;
+var count_Collection;
+var counters_Collection = textileManagerDB.collection("textileManagerDB");
 
 //tis function initializes the database and the collections.
 //if the database does not exist a new database is created, if it exists the existing database is used
-initializeDB();
 function initializeDB(){
 // a collection in Mongo is roughly equalto a table in RDBMS
-var loginCreds_Collection = textileManagerDB.collection("textileManagerDB");
-var	suppliers_Collection = textileManagerDB.collection("textileManagerDB");
-var customers_Collection = textileManagerDB.collection("textileManagerDB");
-var lot_Collection = textileManagerDB.collection("textileManagerDB");
+loginCreds_Collection = textileManagerDB.collection("textileManagerDB");
+suppliers_Collection = textileManagerDB.collection("textileManagerDB");
+customers_Collection = textileManagerDB.collection("textileManagerDB");
+lot_Collection = textileManagerDB.collection("textileManagerDB");
 /*
 LOT will have 
 	i)a unique, preferebally auto_incrementable id
@@ -25,20 +31,19 @@ LOT will have
 	vii) price purchased for
 */
 
-var count_Collection = textileManagerDB.collection("textileManagerDB");
+count_Collection = textileManagerDB.collection("textileManagerDB");
 //will contain a count type and description
 
 
-count_Collection.insert({count:"30s", description:"combed"}, function(err, result) {
+count_Collection.insert({_id:"01", count:"30s", description:"combed"}, function(err, result) {
   assert.equal(null, err);
   console.log(err);
-  console.log(result);
- });
+  });
 
 
 loginCreds_Collection.insert([
-	{name:'admin',passwd:'test'},
-	{name:'guest', passwd:''}],
+	{_id:"admin", name:'admin',passwd:'test'},
+	{_id:"guest", name:'guest', passwd:''}],
     function(err, result) {
   assert.equal(null, err);
   console.log(err);
@@ -46,13 +51,17 @@ loginCreds_Collection.insert([
 
 
 suppliers_Collection.insert(
-	{name:"Mangalam Yarns", tinNo:123456, phoneNo1:"+919952494995", emailId : "naeem.mailto@gmail.com", altPhone:"+910000000", phoneNo2:"+91888888888",  streetAddres:"Palladam Road", state:"TN", city:"mangalam"  },
-	{name:"Mangalam Yarns", tinNo:123456, phoneNo1:"+919952494995", emailId : "naeem.mailto@gmail.com", altPhone:"+910000000", phoneNo2:"+91888888888",  streetAddres:"Palladam Road", state:"TN", city:"mangalam"  }
-	, function(err,result){
+	{_id:"address01", name:"Mangalam Yarns", tinNo:123456, phoneNo1:"+919952494995", emailId : "naeem.mailto@gmail.com", altPhone:"+910000000", phoneNo2:"+91888888888",  streetAddres:"Palladam Road", state:"TN", city:"mangalam"  },
+	function(err,result){
 		assert.equal(null,err);
 	});
 
 }
+
+
+//collection to maintain 'auto_incrementing';
+counters_Collection.insert({_id:"customer_id",seq:1 });
+
 
 
 /*
@@ -61,8 +70,15 @@ add: Whether to add or remove the customer, true denotes add
 cust: the object containing the details of the customer or in case of remove, the id of the customer to be removed
 */
 function addRemCustomer(add,cust){
+
 	if(add == true){
-		customers_Collection.insert(cust);
+		cust=JSON.parse(cust);
+		cust._id=getNextSequence("customer_id");
+		customers_Collection.insert(cust, function(err,result){
+			assert.equal(null,err);
+			console.log(err);
+		});
+
 	}
 	else{
 		customers_Collection.remove({"_id":cust});
@@ -126,4 +142,21 @@ function changePassword(){
 function errorCallback(err, result) {
 	assert.equal(null,err);
 	console.log(err);
+}
+
+
+
+
+function getNextSequence(name) {/*
+	customers_Collection.insert(cust, function(err,result){
+			assert.equal(null,err);
+			console.log(err);
+		});*/
+
+	var ret = counters_Collection.find({'_id':name});
+	counters_Collection.update({'_id':name},{$inc:{'seq':1}});
+	var ret = counters_Collection.find({'_id':name});
+   
+
+   return ret.seq;
 }
